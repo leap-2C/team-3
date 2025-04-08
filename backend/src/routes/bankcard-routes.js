71c0b4501a14e99663d7255f27465b1de82f5f49
24LP0058 24LP0058 <24LP0058@G301-CC0203.local>
2025-04-08 10:22:03 +0800

    BACKEND: Moved some files inside resolvers and update on bankcard-routes

--- backend/src/routes/bankcard-routes.js
+++ backend/src/routes/bankcard-routes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import BankCard from "../schema/bankcard-schema.js";
-import { createBankCard } from "../resolvers/createBankCard.js";
-import { getBankCard } from "../resolvers/getBankCard.js";
-import { patchBankCard } from "../resolvers/patchBankCard.js";
+import { createBankCard } from "../resolvers/bank-card/createBankCard.js";
+import { getBankCard } from "../resolvers/bank-card/getBankCard.js";
+import { patchBankCard } from "../resolvers/bank-card/patchBankCard.js";
 
 const router = express.Router();
 
