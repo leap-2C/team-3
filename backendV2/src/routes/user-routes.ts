daf81a2c7cb6b88a0445bd6a7153a29385606d87
amurlin <e.amurlin@gmail.com>
2025-04-09 12:40:12 +0800

    BACKEND: added user resolvers, routes

---
+++ backendV2/src/routes/user-routes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { getUsername } from "../resolvers/user-profile/get-username";
+import { getUser } from "../resolvers/user-profile/get-user";
+import { getUsers } from "../resolvers/user-profile/get-users";
+import { postUser } from "../resolvers/user-profile/post-user";
+import { putUser } from "../resolvers/user-profile/put-user";
+
+export const userRouter = express.Router();
+
+userRouter.get("/view/:username", getUsername);
+userRouter.get("/current-user/:id", getUser);
+userRouter.get("/explore", getUsers);
+userRouter.post("/:userId", postUser);
+userRouter.patch("/:profileId", putUser);
+
+//HELLOAAA
