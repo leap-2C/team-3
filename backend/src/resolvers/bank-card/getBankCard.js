d91dd5a1ec678b43e9c74668bb7cabc730fd44c7
LETRYKA <helloaryka@gmail.com>
2025-04-09 12:25:56 +0800

    Delete backend directory

--- backend/src/resolvers/bank-card/getBankCard.js
+++
@@ -1,9 +0,0 @@
-export const getBankCard = async (req, res)=>{
-    const { userId } = req.params;
-  try {
-    const bankCards = await BankCard.find({ userID: userId });
-    res.status(200).json({ data: bankCards });
-  } catch (err) {
-    console.log("Error on GET bankcard:", err);
-  }
-}
