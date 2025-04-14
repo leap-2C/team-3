b95d356f304db7ef9d2ac36828487f56641dbcb6
amurlin <e.amurlin@gmail.com>
2025-04-14 11:29:06 +0800

    FRONTEND: added create-profile-page2

---
+++ frontend/src/app/profile/_components/TextArea.tsx
@@ -0,0 +1,43 @@
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import { UseFormRegister, RegisterOptions } from "react-hook-form";
+import { ProfileFormData } from "../page";
+
+interface TextareaGroupProps {
+    id: keyof ProfileFormData;
+    label: string;
+    placeholder: string;
+    className?: string;
+    register: UseFormRegister<ProfileFormData>;
+    registerOptions?: RegisterOptions<ProfileFormData, keyof ProfileFormData>;
+    error?: string;
+  }
+
+export default function TextareaGroup({
+  id,
+  label,
+  placeholder,
+  className = "",
+  register,
+  registerOptions,
+  error,
+}: TextareaGroupProps) {
+  return (
+    <div className={`flex flex-col ${className}`}>
+      <div className="w-full h-[128px] rounded-2xl bg-[#161616] border border-[#3B3B3B] px-4 py-3 flex flex-col justify-start">
+        <Label htmlFor={id} className="text-xs text-white">
+          {label}
+        </Label>
+        <Textarea
+          id={id}
+          placeholder={placeholder}
+          {...register(id, registerOptions)}
+          className="p-0 mt-1 resize-none bg-transparent border-none text-sm text-white focus-visible:ring-0 focus-visible:ring-offset-0"
+        />
+      </div>
+      {error && (
+        <p className="text-red-500 text-xs mt-1 px-2">{error}</p>
+      )}
+    </div>
+  );
+}
