b49856df01ca94aa298d4128e17e2a01833e376f
24LP5396 24LP5396 <24LP5396@G301-CC0193.local>
2025-04-16 10:47:51 +0800

    BACKEND: added express.json edited cors issue

--- backendV2/src/app.ts
+++ backendV2/src/app.ts
@@ -1,25 +1,24 @@
+import { donationRoute } from "./routes/donation-routes";
+import { bankCardRoute } from "./routes/bankcard-routes";
+import { userRoute } from "./routes/user-routes";
+import { authRoute } from "./routes/auth-routes";
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { donationRoute } from "./routes/donation-routes";
-import { userRoute } from "./routes/user-routes";
-import { authRoute } from "./routes/auth-routes";
-import { bankCardRoute } from "./routes/bankcard-routes";
 
 dotenv.config();
 const app = express();
 const prisma = new PrismaClient();
 const port = process.env.PORT || 8000;
 
-app.use(cors());
 const allowedOrigins = ["http://localhost:3000"];
 
 app.use(
   cors({
-    origin: (origin, callback) => {
+    origin: function (origin, callback) {
       if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
+        callback(null, origin);
       } else {
         callback(new Error("Not allowed by CORS"));
       }
@@ -28,15 +27,17 @@ app.use(
   })
 );
 
+app.use(express.json());
+
 app.use("/api/donation", donationRoute);
 app.use("/api/bankcard", bankCardRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/profile", userRoute);
 
-app.listen(port, async () => {
-  console.log(`🟢 Server is running`);
-});
-
 app.get("/api", (req, res) => {
   res.send("API is running...");
+});
+
+app.listen(port, async () => {
+  console.log(`🟢 Server is running on port ${port}`);
 });
