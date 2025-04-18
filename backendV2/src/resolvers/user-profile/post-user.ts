f2a10d0238d930c8f10a944a1aef105c4ff8bfac
amurlin <e.amurlin@gmail.com>
2025-04-18 11:35:27 +0800

    BACKEND: post-user deer data utguudig uurchilsun

--- backendV2/src/resolvers/user-profile/post-user.ts
+++ backendV2/src/resolvers/user-profile/post-user.ts
@@ -3,49 +3,44 @@ import { PrismaClient, User, Profile } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// POST /profile/:userId (Route to create a new profile for the user)
+// POST /profile/:userId 
 export const postUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const {
-      name,
+      username,
+      firstname,
+      lastname,
       about,
       avatarImage,
-      socialMediaURL,
       backgroundImage,
-      successMessage,
     } = req.body;
 
-    // Create a new Profile
     const profile = await prisma.profile.create({
       data: {
-        name,
+        username,
+        firstname,
+        lastname,
         about,
         avatarImage,
-        socialMediaURL,
         backgroundImage,
-        successMessage,
       },
     });
 
-    // Find the user by userId
     const user = await prisma.user.findUnique({
       where: { id: Number(req.params.userId) },
     });
 
-    // If the user is not found, return a 404 error
     if (!user) {
       res.status(404).json({ message: "User not found" });
     } else {
-      // Update the user to associate the profile with the user
       await prisma.user.update({
         where: { id: user.id },
         data: {
-          profileId: profile.id, // Set the profileId of the user
+          profileId: profile.id, 
         },
       });
     }
 
-    // Return the newly created profile as a response
     res.status(201).json(profile);
   } catch (error) {
     res.status(500).json({
